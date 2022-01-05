0ebbd893929819cb600d337ea37c5aa977c68dfe
muriukisam <mr.muriukisammy@gmail.com>
2022-01-05 10:45:07 +0300

    bubble sort

---
+++ bubblesort.py
@@ -0,0 +1,10 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(0, n-1):
+        for j in range(0, n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+    
+ar=[23,45,6,3,567,2,56,7,33,3,77,9,44,5]
+print(bubble_sort(ar))
