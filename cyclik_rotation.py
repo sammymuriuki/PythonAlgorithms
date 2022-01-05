74b4d127131fa4afc60814ff10cf4fef76b37cfe
muriukisam <mr.muriukisammy@gmail.com>
2022-01-05 17:41:01 +0300

    Insertion sort

---
+++ cyclik_rotation.py
@@ -0,0 +1,14 @@
+def cyclic_rotation(k, arr):
+    n = len(arr)
+    if n<1:
+        return arr
+    for i in range(k):
+        temp = arr[n-1]
+        for j in range(n-1, 0, -1):
+            arr[j]=arr[j-1]
+        arr[0]=temp
+    return arr
+
+A = []
+K = 3
+print(cyclic_rotation(K, A))
