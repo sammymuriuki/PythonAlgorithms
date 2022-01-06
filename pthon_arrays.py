2250325e708dd3be8aefd8a43a4c9fc1c3f310ae
muriukisam <mr.muriukisammy@gmail.com>
2022-01-06 21:29:06 +0300

    Python Arrays

--- pthon_arrays.py
+++ pthon_arrays.py
@@ -1,3 +1,9 @@
+'''
+Problem: Given array A consisting of N integers, return the reversed array.
+Solution: We can iterate over the first half of the array and exchange the elements with
+those in the second part of the array.
+'''
+
 def reverse_array(A):
     n = len(A)
     for i in range(n//2):
