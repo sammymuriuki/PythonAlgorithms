290b508313823e40f52b9987b00ab69066d652fa
sammymuriuki <samryuki@gmail.com>
2022-08-02 06:16:32 +0300

    Convert tuple into ditionary

---
+++ converttupleintodictionary.py
@@ -0,0 +1,11 @@
+''' Convert tuple into a dictionary '''
+tuple1 = (('make','toyota'), ('model','landcruiser'), ('YOM','2019'), ('color','black'));
+dict1 = dict();
+for x, y in tuple1:
+    dict1.setdefault(x, []).append(y);
+
+print(dict1);
+''' Method 2 '''
+tuple2 = (('make','toyota'), ('model','landcruiser'), ('YOM','2019'), ('color','black'));
+dict2 = dict(tuple2);
+print(dict2);
---
+++ permcheck.py
@@ -0,0 +1,2 @@
+# https://app.codility.com/demo/results/trainingFVBFWY-V9Q/
+# https://app.codility.com/demo/results/trainingH29BR6-HNU/
