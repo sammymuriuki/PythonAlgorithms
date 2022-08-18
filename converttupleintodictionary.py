5b407910c07e6694d1db3b9030cccf472150dbe5
sammymuriuki <samryuki@gmail.com>
2022-08-18 21:59:38 +0300

    Optimize bubble sort

--- converttupleintodictionary.py
+++ converttupleintodictionary.py
@@ -5,6 +5,7 @@ for x, y in tuple1:
     dict1.setdefault(x, []).append(y);
 
 print(dict1);
+
 ''' Method 2 '''
 tuple2 = (('make','toyota'), ('model','landcruiser'), ('YOM','2019'), ('color','black'));
 dict2 = dict(tuple2);
